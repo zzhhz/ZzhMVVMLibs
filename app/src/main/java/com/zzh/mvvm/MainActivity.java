171308f738e8f49d3cabfa4ca1bee79ed765bfa7
墨宇 <zzh_hz@163.com>
2018-04-16 11:56:06 +0800

    Initial commit

---
+++ app/src/main/java/com/zzh/mvvm/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zzh.mvvm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @date: 2018/4/16 上午11:43
+ * @email: zzh_hz@126.com
+ * @QQ: 1299234582
+ * @author: zzh
+ * @description: MainActivity.java
+ * @since
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
